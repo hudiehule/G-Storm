283d5a6750563acdef0c845f7e3457647bfd871d
hudiehule <1106862480@qq.com>
2017-07-27 10:24:41 +0800

    add an GPUBolt

---
+++ storm-core/src/jvm/org/apache/storm/topology/base/BaseGPUBolt.java
@@ -0,0 +1,11 @@
+package org.apache.storm.topology.base;
+
+import org.apache.storm.topology.IRichBolt;
+
+/**
+ * Created by Administrator on 2017/7/27.
+ * a GPU bolt that can be used to build topology by users
+ */
+public abstract class BaseGPUBolt extends BaseRichBolt implements IRichBolt{
+
+}
